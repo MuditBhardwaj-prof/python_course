5780212701ef7c04cc29ec539b3a705d94faf8a7
MuditBhardwaj-prof <just.sleep.m@gmail.com>
2026-03-29 14:11:07 +0530

    file added

---
+++ FirstProgram.py
@@ -0,0 +1,61 @@
+#arithmetic operators
+a = 5
+b = 2
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b)
+
+#relational operators
+a = 50
+b = 20
+# print(a == b)#False
+# print(a != b)#True
+# print(a >= b)#True
+# print(a > b)#True
+# print(a <= b)#False
+# print(a < b)#False
+
+#assignment operators
+# num = 10
+# # num = num + 10 #10+10 => 20
+# num += 10
+# print(num)
+
+#logical operators
+# print(not False)
+# print(not True)
+# a = 50
+# b = 60
+# print(not (a > b))
+
+# val1 = True
+# val2 = False
+# # print("and operator:", val1 and val2) #False
+# print("OR operator:", val1 or val2)
+
+#Type conversion
+# a = int("2")
+# b = 4.25
+
+# sum = a + b
+# print(sum)
+
+# name = input("Please enter your name: ")
+# print("Welcome ", name)
+
+# val = float(input("please enter some value: "))
+# print(type(val), val)
+
+# num1 = int(input("Please enter first number: "))
+# num2 = int(input("Please enter second number: "))
+# sum = num1 + num2
+# print("The sum is: ", sum)
+
+A = int(input("Please enter first number: "))
+B = int(input("Please enter second number: "))
+print(A >= B)
+
