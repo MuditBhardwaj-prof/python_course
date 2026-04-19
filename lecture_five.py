094bf2e858b71d42b00bdb8a91e28a2dceff08fd
MuditBhardwaj-prof <just.sleep.m@gmail.com>
2026-04-19 23:01:24 +0530

    this file added by me

---
+++ lecture_five.py
@@ -0,0 +1,43 @@
+# while loop
+# count = 1 #iterator
+# while count <= 5:
+#     print("This is an infinite loop")
+#     count = count + 1
+
+# i = 1
+# while i<=10000:
+#     print("welcome Brother"
+#           "how are you?", i)
+#     i += 1
+
+#printing numbers from 1 to 10
+# i = 10
+# while i >= 1:
+#     print(i)
+#     i -= 1
+# print("END OF CODE")
+# printing numbers from 1 to 100
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+# print("END OF CODE")
+
+#now 100 to 1
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+# print("END OF CODE")
+# multiplication table of 5
+# i = 1
+# while i <= 10:
+#     print(5*i)
+#     i += 1
+
+num = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+Idx = 0
+while Idx < len(num):
+    print(num [Idx])
+    Idx += 1
